68b9506b20b3b52aacdc19847079f3d6c0c7bdba
ZairaF9 <98000015+ZairaF9@users.noreply.github.com>
2022-05-24 02:14:47 -0500

    Update AudioRManager.cpp

--- AgataD3D/AudioRManager.cpp
+++ AgataD3D/AudioRManager.cpp
@@ -9,6 +9,8 @@ void AudioRManager::LoadAudioFiles() {
 	Audio::GetInstance().LoadAudioFile((char*)"Assets/Audio/Recoger2.mp3", (char*)"Recoger2", true, 3);
 	Audio::GetInstance().LoadAudioFile((char*)"Assets/Audio/tractor.mp3", (char*)"Tractor", true, 4);
 	Audio::GetInstance().LoadAudioFile((char*)"Assets/Audio/TemaPrincipalR.mp3", (char*)"JuegoR", true, 5);
+	Audio::GetInstance().LoadAudioFile((char*)"Assets/Audio/win.mp3", (char*)"win", true, 6);
+	Audio::GetInstance().LoadAudioFile((char*)"Assets/Audio/perderS.mp3", (char*)"lose", true, 7);
 
 
 }
@@ -19,5 +21,7 @@ void AudioRManager::ReleaseAudioFiles() {
 	Audio::GetInstance().ReleaseSound(Audio::GetInstance().GetSoundsMap().find((char*)"Recoger2")->second);
 	Audio::GetInstance().ReleaseSound(Audio::GetInstance().GetSoundsMap().find((char*)"Tractor")->second);
 	Audio::GetInstance().ReleaseSound(Audio::GetInstance().GetSoundsMap().find((char*)"JuegoR")->second);
+	Audio::GetInstance().ReleaseSound(Audio::GetInstance().GetSoundsMap().find((char*)"win")->second);
+	Audio::GetInstance().ReleaseSound(Audio::GetInstance().GetSoundsMap().find((char*)"lose")->second);
 }
 
